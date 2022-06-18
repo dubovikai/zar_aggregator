dddbfa6d9bc44d92505c30de8d762c7c123b4864
Andrey Dubovik <dai66@mail.ru>
2022-06-18 17:26:47 +0500

    fix: Project Name

--- src/core/settings.py
+++ src/core/settings.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     environment = 'local'
     is_debug = True
 
-    project_title = 'Sandbox Project'
+    project_title = 'ZarAgg'
     project_version = 0
 
     api_token = 'secret'
